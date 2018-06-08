5b7df61b3729c530bbd206139eea76c7c589fdde
Shaurya Sood <shauryasood10@gmail.com>
2018-06-09 03:48:52 +0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    to: 'Andrew',
-    text: '123'
-  });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New User connected');
 
-  socket.emit('newMessage', {
-    from: 'Mike',
-    text: 'Hey',
-    createdAt: 123
-  });
-
-  socket.on('createMessage', (newMessage) => {
-    console.log('Create Message', newMessage);
+  socket.on('createMessage', (message) => {
+    console.log('Create Message', message)
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
